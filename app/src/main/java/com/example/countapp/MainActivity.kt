559a74aca042075a49ee6434d7082f8c84e7cca8
sora <s0ra_kanda@icloud.com>
2019-06-24 19:15:24 +0900

    変数の宣言場所を変更 コメントを追記

--- app/src/main/java/com/example/countapp/MainActivity.kt
+++ app/src/main/java/com/example/countapp/MainActivity.kt
@@ -8,31 +8,42 @@ import android.widget.TextView;
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //変数の宣言
+    var count_number: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var count_number: Int = 0
         setContentView(R.layout.activity_main)
         //countTextViewのインスタンスを取得
         val countTextView = findViewById<TextView>(R.id.number)
 
         //Buttonの処理を追記
+
+        //足し算
         plus.setOnClickListener {
             count_number += 1
             countTextView.setText(count_number.toString())
         }
+
+        //引き算
         minus.setOnClickListener {
             count_number -= 1
             countTextView.setText(count_number.toString())
         }
+
+        //変数初期化
         clear.setOnClickListener {
             count_number = 0
             countTextView.setText(count_number.toString())
         }
+
+        //掛け算
         kakeru.setOnClickListener {
             count_number *= 2
             countTextView.setText(count_number.toString())
         }
+
+        //割り算
         waru.setOnClickListener {
             count_number /= 2
             countTextView.setText(count_number.toString())
